d65b753fffaa0b803834a40377faf81d9327e408
CanadianBeaver <CanadianBeaver@users.noreply.github.com>
2023-03-05 01:59:52 -0500

    douwgraded

--- Articles/.vuepress/config.js
+++ Articles/.vuepress/config.js
@@ -1,68 +1,70 @@
-import { defineUserConfig } from 'vuepress'
-import { defaultTheme } from 'vuepress'
-import { searchPlugin } from '@vuepress/plugin-search'
-
-export default defineUserConfig({
+module.exports = {
     lang: 'en-US',
     title: 'Awesome Factorio',
     description: 'How to build an awesome factory? It is over 9,000 science packs per minute...',
     base: '/AwesomeFactorio/',
 
     locales: {
-        '/': {
-            lang: 'en-US',
-        },
-        '/ru/': {
-            lang: 'ru-RU',
-            title: 'Прекрасная Фактория',
-            description: 'Как построить классную фабрику? Это 100500 научных пакетов в минуту...',
-        }
+      '/': {
+        lang: 'en-US',
+      },
+      '/ru/': {
+        lang: 'ru-RU',
+        title: 'Прекрасная Фактория',
+        description: 'Как построить классную фабрику? Это 100500 научных пакетов в минуту...',
+      }
     },
 
-    theme: defaultTheme({
-        logo: '/assets/img/logo.svg',
-        displayAllHeaders: true,
-        editLink: false,
-        lastUpdated: false,
-        contributors: false,
-        locales: {
-            '/': {
-                sidebar: [
-                    {
-                        title: 'Power production',
-                        path: '/PowerProduction/PowerProduction',
-                        collapsable: false,
-                        sidebarDepth: 0,
-                        children: [
-                            '/PowerProduction/SteamPower',
-                            '/PowerProduction/SolarPower',
-                            '/PowerProduction/NuclearPower'
-                        ]
-                    },
-                    '/LoadingAndUnloadingTrains'
-                ]
+    themeConfig: {
+      logo: '/assets/img/logo.svg',
+      displayAllHeaders: true,
+      editLinks: false,
+      lastUpdated: false,
+      displayAllHeaders: true,
+      smoothScroll: true,
+      locales: {
+        '/': {
+          label: 'English',
+          sidebar: [
+            {
+              title: 'Power production',
+              path: '/PowerProduction/PowerProduction',
+              collapsable: false,
+              sidebarDepth: 0,
+              children: [
+                '/PowerProduction/SteamPower',
+                '/PowerProduction/SolarPower',
+                '/PowerProduction/NuclearPower'
+              ]
             },
-            '/ru/': {
-                selectLanguageName: 'Русский',
-                sidebar: [
-                    {
-                        title: 'Производство энергии',
-                        path: '/ru/PowerProduction/PowerProduction',
-                        collapsable: false,
-                        sidebarDepth: 0,
-                        children: [
-                            '/ru/PowerProduction/SteamPower',
-                            '/ru/PowerProduction/SolarPower',
-                            '/ru/PowerProduction/NuclearPower'
-                        ]
-                    },
-                    '/ru/LoadingAndUnloadingTrains'
-                ]
-            }
+            '/LoadingAndUnloadingTrains'
+          ]
+        },
+        '/ru/': {
+          label: 'Русский',
+          sidebar: [
+            {
+              title: 'Производство энергии',
+              path: '/ru/PowerProduction/PowerProduction',
+              collapsable: false,
+              sidebarDepth: 0,
+              children: [
+                '/ru/PowerProduction/SteamPower',
+                '/ru/PowerProduction/SolarPower',
+                '/ru/PowerProduction/NuclearPower'
+              ]
+            },
+            '/ru/LoadingAndUnloadingTrains'
+          ]
         }
-    }),
+      }
+
+    },
 
     plugins: [
-        searchPlugin(),
-    ],
-})
+      '@vuepress/plugin-back-to-top',
+      '@vuepress/plugin-medium-zoom',
+      '@vuepress/search',
+      '@vuepress/active-header-links'
+    ]
+  }
