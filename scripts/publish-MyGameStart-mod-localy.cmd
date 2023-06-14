4c33e8e715ee60d6b26f859ce992a4303fa0c82c
CanadianBeaver <CanadianBeaver@users.noreply.github.com>
2023-06-14 19:41:39 -0400

    removed old modifications

--- scripts/publish-MyGameStart-mod-localy.cmd
+++
@@ -1,3 +0,0 @@
-REM if not exist ./build mkdir build
-REM powershell Compress-Archive ./src/MyGameStart ./build/MyGameStart_2020.10.0.zip -Force
-powershell Compress-Archive ./src/MyGameStart %appdata%/Factorio/mods/MyGameStart_2020.10.0.zip -Force
