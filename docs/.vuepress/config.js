35456f4a015065609a22d683ec38a8ca2d9fa8b0
CanadianBeaver <CanadianBeaver@users.noreply.github.com>
2023-06-27 00:31:13 -0400

    google analitics

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -124,5 +124,19 @@ module.exports = {
             headerless: true
           }
       }
-  }
+  },
+
+  head: [
+    ['script', {
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-P1GFSP4DQK',
+        async: true
+    }],
+    ['script', {}, `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-P1GFSP4DQK');
+    `],
+  ],
+
 }
