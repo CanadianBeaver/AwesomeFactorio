bd49cc7f695915e70984b7577736b33d87dec241
CanadianBeaver <CanadianBeaver@users.noreply.github.com>
2024-03-30 20:13:59 -0400

    updated images

--- website/sidebars.ts
+++ website/sidebars.ts
@@ -36,7 +36,7 @@ const sidebars: SidebarsConfig = {
         type: 'doc',
         id: 'RawResourcesProcessing/README',
       },
-      items: []
+      items: ['RawResourcesProcessing/BigOreFoundry']
     },
     {
       type: 'category',
