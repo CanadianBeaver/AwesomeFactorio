5a0da2b67fb38c089a85ce333d2f96333c7ddc3f
CanadianBeaver <CanadianBeaver@users.noreply.github.com>
2025-05-19 16:44:25 -0400

    draft of TipsAndTricks article

--- website/sidebars.ts
+++ website/sidebars.ts
@@ -11,6 +11,7 @@ const sidebars: SidebarsConfig = {
         id: 'HowToStartNewGame/README',
       },
       items: [
+        'HowToStartNewGame/TipsAndTricks',
         'HowToStartNewGame/BurnerDevices',
         'HowToStartNewGame/ManualConstruction',
         'HowToStartNewGame/Mall',
@@ -97,7 +98,9 @@ const sidebars: SidebarsConfig = {
         type: 'doc',
         id: 'HowToStartNewGame/README'
       },
-      items: []
+      items: [
+        'HowToStartNewGame/TipsAndTricks'
+      ]
     }
   ],
 
