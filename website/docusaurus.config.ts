e785812ae83201bd95c66166ff00ef1d34f4e8ca
CanadianBeaver <CanadianBeaver@users.noreply.github.com>
2025-06-07 02:02:39 -0400

    ADDED GTAG

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -251,6 +251,7 @@ const config: Config = {
     require.resolve("docusaurus-plugin-image-zoom"),
     ['docusaurus-plugin-yandex-metrica', { counterID: '97640344' }],
     ['@docusaurus/plugin-google-tag-manager', { containerId: 'GTM-TVJLMXS7' }],
+    ['@docusaurus/plugin-google-gtag', { trackingID: 'G-ZCL1B8TRSM', anonymizeIP: true, }],
   ]
 
 };
